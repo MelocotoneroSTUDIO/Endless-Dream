2bcbb841ecb56b9e15d896a697fa8d18ee455e9d
Victor Cabanillas <115984937+VictorCabanillas@users.noreply.github.com>
2023-09-28 16:15:08 +0200

    Breaking Bloock shake

--- VideojuegoWeb/Assets/Scripts/BreakingBlock.cs
+++ VideojuegoWeb/Assets/Scripts/BreakingBlock.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class BreakingBlock : InteractableObject
 {
     public int breakResistance;
     private int hits=0;
+    public float shakeStrength = 1;
+    public float shakeTime = 1;
 
     public override void Interact()
     {
         hits++;
+        transform.DOShakePosition(shakeTime,shakeStrength);
         if (hits >= breakResistance) 
         {
         Destroy(gameObject);
