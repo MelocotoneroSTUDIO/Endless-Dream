8539deff718f8926b8e16014b234fc7f50afd680
Victor Cabanillas <115984937+VictorCabanillas@users.noreply.github.com>
2023-10-01 17:39:25 +0200

    Basic sheep

--- VideojuegoWeb/Assets/Scripts/Mechanics/Sheeps/SheepBehaviour.cs
+++ VideojuegoWeb/Assets/Scripts/Mechanics/Sheeps/SheepBehaviour.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -26,6 +27,7 @@ public class SheepBehaviour : InteractableObject
         else 
         {
             StopCoroutine(destinationCorutine);
+            destinationCorutine = null;
         }
     }
 
