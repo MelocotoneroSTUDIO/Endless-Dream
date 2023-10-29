66849ff93dad77335a24d7fc3011d13a61ac00ab
VictorCabanillas <vector.uno4@gmail.com>
2023-10-29 18:10:43 +0100

    Starting Data collector system with some code planning

---
+++ VideojuegoWeb/Assets/Scripts/DataCollector/DataCollector.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataCollector : MonoBehaviour
+{
+    //Data variables updated onruntime
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Need method/s to update current data
+
+    public void SaveData() 
+    {
+        //Called at the end of the level
+        //Needs to call static data saver to upload current data to the txt or database
+    }
+}
---
+++ VideojuegoWeb/Assets/Scripts/DataCollector/DataSaver.cs
@@ -0,0 +1,8 @@
+public static class DataSaver
+{
+    public static void SaveData() 
+    {
+        //Upload data to txt or database (if connection available?)
+        //Might need a serializable data class to store all data and transfer it
+    }
+}
