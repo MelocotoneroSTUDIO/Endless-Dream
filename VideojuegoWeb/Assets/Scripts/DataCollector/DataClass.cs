a3a5a561e5a732966d3480dbcf982e64793e16e8
Victor Cabanillas <115984937+VictorCabanillas@users.noreply.github.com>
2023-11-04 20:02:21 +0100

    Data saver now also takes in  Gender and Age data from PData file

--- VideojuegoWeb/Assets/Scripts/DataCollector/DataClass.cs
+++ VideojuegoWeb/Assets/Scripts/DataCollector/DataClass.cs
@@ -4,6 +4,8 @@ using System;
 public class DataClass
 {
     //Add current level info
+    public string gender = null;
+    public int age = 0;
     public string level = "";
     public int interactions = 0;
     public float time = 0;
@@ -13,9 +15,11 @@ public class DataClass
 
     public string DataToString() 
     {
-        string result;
+        string result="";
 
-        result = "Level: " + level + "\n";
+        result = result + "Age: " + age + "\n";
+        result = result + "Gender: " + gender + "\n";
+        result = result + "Level: " + level + "\n";
         result = result + "Interacions: " + interactions + "\n";
         result = result + "Time: " + time + "\n";
         result = result + "Coins: " + coins + "\n";
--- VideojuegoWeb/Assets/Scripts/DataCollector/DataSaver.cs
+++ VideojuegoWeb/Assets/Scripts/DataCollector/DataSaver.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public static class DataSaver
 {
-    static string path = Application.streamingAssetsPath + "/Data/";
+    static string writepath = Application.streamingAssetsPath + "/Data/Levels/";
+    static string readpath = Application.streamingAssetsPath + "/Data/Personal/PData.txt";
     public static void SaveData(DataClass data) 
     {
         Debug.Log(Application.streamingAssetsPath);
+        //Missing add gender and age from save
+        using (StreamReader reader = new StreamReader(readpath)) 
+        {
+            data.gender = reader.ReadLine();
+            data.age = int.Parse(reader.ReadLine());
+        }
+
         //Upload data to txt or database (if connection available?)
         //Might need a serializable data class to store all data and transfer it
-        using (StreamWriter outputFile = new StreamWriter(Path.Combine(path, "data.txt")))
+        using (StreamWriter outputFile = new StreamWriter(Path.Combine(writepath, "data.txt")))
         {
-                outputFile.WriteLine(data.DataToString());
+            outputFile.WriteLine(data.DataToString());
         }
     }
 }
