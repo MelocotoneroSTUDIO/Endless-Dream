dfcb14a89471bf52d5afa891d83f66c6ff37720f
AdrianaSanchezIllan <a.sanchezi.2020@alumnos.urjc.es>
2023-11-16 21:57:37 +0100

    Metiendo mundo 1 dientes + interfaz pantalla juego

---
+++ VideojuegoWeb/Assets/MundoDientes/Shaders/ObjectsPosition.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shader.SetGlobalVector("ObjectsPosition", new Vector4(this.transform.position.x, this.transform.position.y, this.transform.position.z, this.transform.localScale.x));
+    }
+}
+
