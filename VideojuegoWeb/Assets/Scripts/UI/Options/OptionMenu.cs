26e90bdf76d95e0cf21e0606254955b2768ac69f
Melocotonero Studio <aisayan68@gmail.com>
2023-11-17 03:38:49 +0100

    Menu de opciones portable

--- VideojuegoWeb/Assets/Scripts/UI/Options/OptionMenu.cs
+++ VideojuegoWeb/Assets/Scripts/UI/Options/OptionMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class OptionMenu : MonoBehaviour
@@ -10,13 +11,19 @@ public class OptionMenu : MonoBehaviour
     [SerializeField] TextMeshProUGUI volumenText;
     [SerializeField] Button volumeUp;
     [SerializeField] Button volumeDown;
+    [SerializeField] AudioMixer audioMixer;
     int currentVolume;
-
+    
 
     private void Awake()
     {
+        currentVolume = 0;
+        ChangeVolume(5);
+        
         volumeUp.onClick.AddListener(() => ChangeVolume(1));
         volumeDown.onClick.AddListener(() => ChangeVolume(-1));
+
+        Hide();
     }
 
     void ChangeVolume(int volumen)
@@ -33,5 +40,18 @@ public class OptionMenu : MonoBehaviour
         }
 
         volumenText.text = currentVolume.ToString();
+
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(currentVolume)*20);
+
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
     }
 }
