63c153f1355e63df8a04132595b30f31344aa242
VictorCabanillas <vector.uno4@gmail.com>
2023-11-19 22:44:43 +0100

    removing usings

--- VideojuegoWeb/Assets/Scripts/UIClickSoundButton.cs
+++ VideojuegoWeb/Assets/Scripts/UIClickSoundButton.cs
@@ -3,11 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
 [RequireComponent(typeof(Button))]
 public class UIClickSoundButton : MonoBehaviour
 {
