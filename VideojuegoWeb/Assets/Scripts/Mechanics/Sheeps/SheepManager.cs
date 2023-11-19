7bfc92f64f8f3ca4ff3f0731c8addeb144ecec8b
rosasuffo <rositasuffo@gmail.com>
2023-11-19 22:27:02 +0100

    cambiando la letra

--- VideojuegoWeb/Assets/Scripts/Mechanics/Sheeps/SheepManager.cs
+++ VideojuegoWeb/Assets/Scripts/Mechanics/Sheeps/SheepManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SheepManager : MonoBehaviour
 {
     //Class that manages all sheeps in the level
-    private List<SheepBehaviour> sheeps = new List<SheepBehaviour>();
+    [SerializeField] private List<SheepBehaviour> sheeps = new List<SheepBehaviour>();
     public Transform playerFollow;
 
     public SheepBehaviour getFollowSheep() 
