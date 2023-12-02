fb58fa37360d1d4c1cc16ef6369910416b684971
rosasuffo <rositasuffo@gmail.com>
2023-12-02 23:43:22 +0100

    nivel2
    un par de cosas pa ir montandolo

--- VideojuegoWeb/Assets/Scripts/Mechanics/Interactable/AutoMovingObject.cs
+++ VideojuegoWeb/Assets/Scripts/Mechanics/Interactable/AutoMovingObject.cs
@@ -14,23 +14,17 @@ public class AutoMovingObject : MonoBehaviour
     public float timeStatic;
     private Vector3 endPosition;
     private Vector3 startPos;
-    //public AudioClip movingSoundClip; // El clip de sonido que deseas reproducir
-    private AudioSource audioSource; 
 
     private void Start()
     {
         startPos = transform.position;
         endPosition = endPos.position;
 
-        audioSource = GetComponent<AudioSource>();
-
         Move(endPosition);
     }
 
     private void Move(Vector3 pos)
     {
-        //audioSource.clip = movingSoundClip;
-        audioSource.Play(); 
         transform.parent.transform.DOMove(pos, timeMoving).OnComplete(() => {
             DOVirtual.DelayedCall(timeStatic, () => Move((pos == endPosition) ? startPos : endPosition));
         });
